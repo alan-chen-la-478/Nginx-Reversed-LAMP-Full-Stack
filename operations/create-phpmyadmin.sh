7ef7f8c347cb7dff96ee645167f62a3e00f874b1
Alan Chen <alan.chen.la.478@gmail.com>
2023-03-23 20:38:21 -0700

    More fixes

--- operations/create-phpmyadmin.sh
+++ operations/create-phpmyadmin.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [[ $DOMAIN == *"phpmyadmin."* ]]; then
+if [ "$PHPMYADMIN" = true ] ; then
+    sudo rm -rf $SERVED_PATH/*
+
     PHPMYADMIN_VERSION=$(curl -s https://www.phpmyadmin.net/downloads/ | grep -o -E 'phpMyAdmin-[0-9]+\.[0-9]+\.[0-9]+' | head -n 1 | cut -c 12-)
     echo "$(tput setaf 2)$(tput bold)Installing phpMyAdmin... $(tput sgr 0)"
-    phpmyadminLocation="https://files.phpmyadmin.net/phpMyAdmin/PHPMYADMIN_VERSION/phpMyAdmin-PHPMYADMIN_VERSION-all-languages.zip"
-    phpmyadminFolderName="phpMyAdmin-PHPMYADMIN_VERSION-all-languages"
+    phpmyadminLocation="https://files.phpmyadmin.net/phpMyAdmin/${PHPMYADMIN_VERSION}/phpMyAdmin-${PHPMYADMIN_VERSION}-all-languages.zip"
+    phpmyadminFolderName="phpMyAdmin-${PHPMYADMIN_VERSION}-all-languages"
 
     sudo wget $phpmyadminLocation -O phpmyadmin.zip
     sudo unzip -q phpmyadmin.zip -d phpmyadmin
